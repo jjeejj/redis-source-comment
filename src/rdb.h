4ccde37fd8816a1fd2496264d2bbc0419e3aeca4
jjeejj <jwj@wenjunjiang.com>
2021-09-08 20:47:07 +0800

    rdb

--- src/rdb.h
+++ src/rdb.h
@@ -64,31 +64,31 @@
 /* When a length of a string object stored on disk has the first two bits
  * set, the remaining six bits specify a special encoding for the object
  * accordingly to the following defines: */
-#define RDB_ENC_INT8 0        /* 8 bit signed integer */
-#define RDB_ENC_INT16 1       /* 16 bit signed integer */
-#define RDB_ENC_INT32 2       /* 32 bit signed integer */
-#define RDB_ENC_LZF 3         /* string compressed with FASTLZ */
+#define RDB_ENC_INT8 0  /* 8 bit signed integer */
+#define RDB_ENC_INT16 1 /* 16 bit signed integer */
+#define RDB_ENC_INT32 2 /* 32 bit signed integer */
+#define RDB_ENC_LZF 3   /* string compressed with FASTLZ */
 
 /* Map object types to RDB object types. Macros starting with OBJ_ are for
  * memory storage and may change. Instead RDB types must be fixed because
  * we store them on disk. */
 #define RDB_TYPE_STRING 0
-#define RDB_TYPE_LIST   1
-#define RDB_TYPE_SET    2
-#define RDB_TYPE_ZSET   3
-#define RDB_TYPE_HASH   4
+#define RDB_TYPE_LIST 1
+#define RDB_TYPE_SET 2
+#define RDB_TYPE_ZSET 3
+#define RDB_TYPE_HASH 4
 #define RDB_TYPE_ZSET_2 5 /* ZSET version 2 with doubles stored in binary. */
 #define RDB_TYPE_MODULE 6
-#define RDB_TYPE_MODULE_2 7 /* Module value with annotations for parsing without
+#define RDB_TYPE_MODULE_2 7 /* Module value with annotations for parsing without \
                                the generating module being loaded. */
 /* NOTE: WHEN ADDING NEW RDB TYPE, UPDATE rdbIsObjectType() BELOW */
 
 /* Object types for encoded objects. */
-#define RDB_TYPE_HASH_ZIPMAP    9
-#define RDB_TYPE_LIST_ZIPLIST  10
-#define RDB_TYPE_SET_INTSET    11
-#define RDB_TYPE_ZSET_ZIPLIST  12
-#define RDB_TYPE_HASH_ZIPLIST  13
+#define RDB_TYPE_HASH_ZIPMAP 9
+#define RDB_TYPE_LIST_ZIPLIST 10
+#define RDB_TYPE_SET_INTSET 11
+#define RDB_TYPE_ZSET_ZIPLIST 12
+#define RDB_TYPE_HASH_ZIPLIST 13
 #define RDB_TYPE_LIST_QUICKLIST 14
 #define RDB_TYPE_STREAM_LISTPACKS 15
 /* NOTE: WHEN ADDING NEW RDB TYPE, UPDATE rdbIsObjectType() BELOW */
@@ -97,32 +97,32 @@
 #define rdbIsObjectType(t) ((t >= 0 && t <= 7) || (t >= 9 && t <= 15))
 
 /* Special RDB opcodes (saved/loaded with rdbSaveType/rdbLoadType). */
-#define RDB_OPCODE_MODULE_AUX 247   /* Module auxiliary data. */
-#define RDB_OPCODE_IDLE       248   /* LRU idle time. */
-#define RDB_OPCODE_FREQ       249   /* LFU frequency. */
-#define RDB_OPCODE_AUX        250   /* RDB aux field. */
-#define RDB_OPCODE_RESIZEDB   251   /* Hash table resize hint. */
-#define RDB_OPCODE_EXPIRETIME_MS 252    /* Expire time in milliseconds. */
-#define RDB_OPCODE_EXPIRETIME 253       /* Old expire time in seconds. */
-#define RDB_OPCODE_SELECTDB   254   /* DB number of the following keys. */
-#define RDB_OPCODE_EOF        255   /* End of the RDB file. */
+#define RDB_OPCODE_MODULE_AUX 247    /* Module auxiliary data. */
+#define RDB_OPCODE_IDLE 248          /* LRU idle time. 空闲时间 */
+#define RDB_OPCODE_FREQ 249          /* LFU frequency. 访问频率信息*/
+#define RDB_OPCODE_AUX 250           /* RDB aux field. */
+#define RDB_OPCODE_RESIZEDB 251      /* Hash table resize hint.  */
+#define RDB_OPCODE_EXPIRETIME_MS 252 /* Expire time in milliseconds. */
+#define RDB_OPCODE_EXPIRETIME 253    /* Old expire time in seconds. */
+#define RDB_OPCODE_SELECTDB 254      /* DB number of the following keys. 数据库编号*/
+#define RDB_OPCODE_EOF 255           /* End of the RDB file. 结束标识*/
 
 /* Module serialized values sub opcodes */
-#define RDB_MODULE_OPCODE_EOF   0   /* End of module value. */
-#define RDB_MODULE_OPCODE_SINT  1   /* Signed integer. */
-#define RDB_MODULE_OPCODE_UINT  2   /* Unsigned integer. */
-#define RDB_MODULE_OPCODE_FLOAT 3   /* Float. */
-#define RDB_MODULE_OPCODE_DOUBLE 4  /* Double. */
-#define RDB_MODULE_OPCODE_STRING 5  /* String. */
+#define RDB_MODULE_OPCODE_EOF 0    /* End of module value. */
+#define RDB_MODULE_OPCODE_SINT 1   /* Signed integer. */
+#define RDB_MODULE_OPCODE_UINT 2   /* Unsigned integer. */
+#define RDB_MODULE_OPCODE_FLOAT 3  /* Float. */
+#define RDB_MODULE_OPCODE_DOUBLE 4 /* Double. */
+#define RDB_MODULE_OPCODE_STRING 5 /* String. */
 
 /* rdbLoad...() functions flags. */
-#define RDB_LOAD_NONE   0
-#define RDB_LOAD_ENC    (1<<0)
-#define RDB_LOAD_PLAIN  (1<<1)
-#define RDB_LOAD_SDS    (1<<2)
+#define RDB_LOAD_NONE 0
+#define RDB_LOAD_ENC (1 << 0)
+#define RDB_LOAD_PLAIN (1 << 1)
+#define RDB_LOAD_SDS (1 << 2)
 
 #define RDB_SAVE_NONE 0
-#define RDB_SAVE_AOF_PREAMBLE (1<<0)
+#define RDB_SAVE_AOF_PREAMBLE (1 << 0)
 
 int rdbSaveType(rio *rdb, unsigned char type);
 int rdbLoadType(rio *rdb);
